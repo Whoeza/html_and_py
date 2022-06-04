0bf05330d234ff505d09e548fa2a4b5408cd35dd
Whoeza <84461456+Whoeza@users.noreply.github.com>
2022-06-04 17:50:34 +0200

    changes to `__all__`

--- src/html_and_py/__init__.py
+++ src/html_and_py/__init__.py
@@ -1,7 +1,11 @@
-all = ["HTML_PROTOTYPE",
-       "HTML_VOID_ELEMENTS",
-       "create_html",
-       "is_self_enclosing",
-       "render_html",
-       "update_html"
-]
+from src.html_and_py.html_and_py \
+    import HTML_PROTOTYPE, HTML_VOID_ELEMENTS, \
+    create_html, is_self_enclosing, render_html, update_html
+
+__all__ = ["HTML_PROTOTYPE",
+           "HTML_VOID_ELEMENTS",
+           "create_html",
+           "is_self_enclosing",
+           "render_html",
+           "update_html"
+           ]
